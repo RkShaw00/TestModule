4150d0a4c3ff3d04fa7910abef590a0f82822eca
RahulShaw <rahul.shaw@tigeranalytics.com>
2022-09-22 12:36:01 +0530

    devopsterraformpractice3

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_app_service_plan" "example" {
 
 
 resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
+  name                = "testdevops-app-service"
   location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
